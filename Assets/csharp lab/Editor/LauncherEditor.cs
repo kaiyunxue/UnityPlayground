3ee5fbde61b68bcad94a626e382f43974727385c
KevinXue <kevinxue0239@gmail.com>
2019-02-05 22:00:50 -0500

    initial

---
+++ Assets/csharp lab/Editor/LauncherEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Launcher))]
+public class LauncherEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        var launcher = target as Launcher;
+        var transform = launcher.transform;
+        launcher.offset = transform.InverseTransformPoint(
+            Handles.PositionHandle(
+                transform.TransformPoint(launcher.offset),
+                transform.rotation));
+    }
+}
---
+++ Assets/csharp lab/scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [HideInInspector]
+    public new Rigidbody rigidbody;
+    void Reset()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+}
